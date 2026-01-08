aa16b805a75e5ce3b7ca6e3ad02768dac22ee1bf
stmacoli <st.macoli@gmail.com>
2026-01-08 17:14:12 -0300

    first_commit_python_alura

---
+++ app.py
@@ -0,0 +1,11 @@
+print("""
+🅂 🄰 🄱 🄾 🅁   🄴 🅇 🄿 🅁 🄴 🅂 🅂
+""")
+print('1. Cadastrar restaurante')
+print('2. Listar restaurantes')
+print('3. Ativar restaurante')
+print('4. Sair \n')
+
+opcao_escolhida = input('Escolha uma opção: ')
+
+print(f'Você escolheu a opção {opcao_escolhida}')
---
+++ exercicios.py
@@ -0,0 +1,17 @@
+# print('Python na Escola de Programação da \nA\nl\nu\nr\na')
+print("""Python na Escola de Programação da 
+      A
+      l
+      u
+      r
+      a""")
+
+
+nome = 'João'
+idade = 22
+
+print(f'Meu é {nome} e tenho {idade} anos')
+
+pi = 3.14159
+
+print(round(pi, 2))
